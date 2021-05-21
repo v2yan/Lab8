1b0470916cf62ef6f91d49cb81902554c4bb9522
Viv <v2yan@ucsd.edu>
2021-05-21 15:31:19 -0700

    finish part 1

--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -2,3 +2,28 @@
  * @jest-environment jsdom
  */
 
+const { pushToHistory } = require("../scripts/router");
+
+describe('pushToHistory function', () => {
+    test('add settings state object to history', () => {
+        const hist = pushToHistory('settings', 19);
+        const currStateObj = hist.state;
+        expect(currStateObj).toEqual({ page: 'settings' });
+        expect(hist.length).toBe(2);
+    });
+    
+    test('add entry state object to history', () => {
+        const hist = pushToHistory('entry', 19);
+        const currStateObj = hist.state;
+        expect(currStateObj).toEqual({ page: 'entry19'});
+        expect(hist.length).toBe(3);
+    });
+    
+    test('add default state object to history', () => {
+        const hist = pushToHistory('', 19);
+        const currStateObj = hist.state;
+        expect(currStateObj).toEqual({});
+        expect(hist.length).toBe(4);
+    });
+}); 
+
--- __tests__/sum.test.js
+++ __tests__/sum.test.js
@@ -1,1 +1,16 @@
 // sum.test.js
+
+import { sum } from '../scripts/sum.js';
+
+// test w/o function 
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1+2).toBe(3);
+});
+
+
+// test sum function
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(3);
+});
